d9a30752a4070a97dd3ca4858bce3ee6cf6ba645
Andreas Maier <andreas.r.maier@gmx.de>
2021-04-12 13:54:11 +0200

    Docs: Fixed development status of Pypi package to be Beta
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ setuptools.setup(
 
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
